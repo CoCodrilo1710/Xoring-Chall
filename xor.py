c421b2b8b6e196affbb9e3545a3b663f0227ec44
Iliescu Bogdan <58045143+CoCodrilo1710@users.noreply.github.com>
2021-11-30 10:49:16 +0200

    updated exceptions

--- xor.py
+++ xor.py
@@ -11,37 +11,40 @@ def urmatoarea_litera():
     while True:
         yield s2[random.randint(0, lungime)]
 
+try:
 
-s1 = sys.argv[1]  # encrypt / decrypt
-s2 = sys.argv[2]  # pass
-s3 = sys.argv[3]  # input
-s4 = sys.argv[4]  # output
+    s1 = sys.argv[1]  # encrypt / decrypt
+    s2 = sys.argv[2]  # pass
+    s3 = sys.argv[3]  # input
+    s4 = sys.argv[4]  # output
 
-parola = urmatoarea_litera()
+    parola = urmatoarea_litera()
 
-if s1 == "encrypt":
+    if s1 == "encrypt":
 
-    f = open(f"{s3}")
-    g = open(f"{s4}", "w")
-    s = f.read()
+        f = open(f"{s3}")
+        g = open(f"{s4}", "w")
+        s = f.read()
 
-    a_list = [bin(ord(a) ^ ord(next(parola)))[2:].zfill(8) for a in s]
-    g.writelines("".join(a_list))
-    f.close()
-    g.close()
+        a_list = [bin(ord(a) ^ ord(next(parola)))[2:].zfill(8) for a in s]
+        g.writelines("".join(a_list))
+        f.close()
+        g.close()
 
-else:
-    f = open(f"{s3}")
-    g = open(f"{s4}", "w")
-    s = f.read()
+    else:
+        f = open(f"{s3}")
+        g = open(f"{s4}", "w")
+        s = f.read()
 
-    decoded = []
-    while len(s) > 0:
-        decodeS = s[:8]
-        ascii = int(decodeS, 2)
-        decoded.append(chr(ascii ^ ord(next(parola))))
-        s = s[8:]
+        decoded = []
+        while len(s) > 0:
+            decodeS = s[:8]
+            ascii = int(decodeS, 2)
+            decoded.append(chr(ascii ^ ord(next(parola))))
+            s = s[8:]
 
-    g.write("".join(decoded))
-    f.close()
-    g.close()
+        g.write("".join(decoded))
+        f.close()
+        g.close()
+except IndexError:
+    raise SystemExit(f"Usage: python3  {sys.argv[0]} encrypt/decrypt key input output ")
